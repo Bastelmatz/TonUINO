65c9379ea4c5c69a09004ecabb235ff44fdffa30
Bastelmatz <74304347+Bastelmatz@users.noreply.github.com>
2021-02-27 22:27:57 +0100

    Add support for standby timer and stop LED to Mira Troy.

--- Tonuino_Main/src/Tonuino_Mira_Troy.cpp
+++ Tonuino_Main/src/Tonuino_Mira_Troy.cpp
@@ -18,6 +18,12 @@ void setupMira()
 {
 	TonuinoConfig config = getConfigMira();
 	
+	config.PINS.Shutdown = 5; // 7
+	config.PINS.StopLED = 7; // 6
+	
+	config.HW.PowerOff = true;
+	
+	config.SW.StandbyTimeInMin = 30;
 	config.SW.VolumeInit = 24; // volume is controlled externally, same value as max somehow crashed dfplayer?!
 	
 	setupTonuino(config);
